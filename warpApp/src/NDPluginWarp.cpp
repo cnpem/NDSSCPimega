94a9ca1e9a8f92e95404cf3b1cb9a1300ec32dd9
Marco A. B. Montevechi Filho <marco.filho@lnls.br>
2023-08-16 19:43:53 -0300

    Strip NDWarp from its core processing. Add simple proof of concept.
    NDWarp now doesnt warps anything.
    Its processCallbacks has been stripped of warping capabilities. Now, the only thing it
    does is setting the first 4718592 bytes (half of an 8-bytes-per-pixel image of 3072x3072 pixels)
    to 1.
    
    So we have a proof of concept of something that receives an array, processes it and returns it back
    to plugins.
    
    Its kinda ready to receive SSCPiMega API, but we still need to clean LOTS of stuff:
    
        - old .db files are probably mainly useless now.
        - ndutil.h is probably useless now.
        - asyn writeInt and WriteFloat tc. functions probably need to be cleaned too.
        - Everything thats called NDWarp should be called NDSSCPiMega or something similar.

--- warpApp/src/NDPluginWarp.cpp
+++ warpApp/src/NDPluginWarp.cpp
@@ -7,25 +7,14 @@
 #include <algorithm>
 #include <limits>
 
+#include <string.h>
+
 #include <epicsMath.h>
 
 #include <iocsh.h>
 #include <epicsExport.h>
 
 #include <NDPluginWarp.h>
-
-// pi/180
-#define PI_180 0.017453292519943295
-
-static
-bool sameShape(const NDArrayInfo& lhs, const NDArrayInfo& rhs)
-{
-    // assumes previous check ndim is 2 or 3
-    return lhs.nElements==rhs.nElements
-            && lhs.xSize==rhs.xSize
-            && lhs.ySize==rhs.ySize
-            && lhs.colorSize==rhs.colorSize;
-}
 
 NDPluginWarp::NDPluginWarp(const char *portName, int queueSize, int blockingCallbacks,
              const char *NDArrayPort, int NDArrayAddr,
@@ -109,7 +98,8 @@ NDPluginWarp::processCallbacks(NDArray *pArray)
     NDPluginDriver::beginProcessCallbacks(pArray);
 
     NDArrayInfo info;
-    (void)pArray->getInfo(&info);
+    NDArray *pOutput;
+    //(void)pArray->getInfo(&info);
 
     if(pArray->ndims!=2 || info.xSize==0 || info.ySize==0) {
         asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
@@ -118,18 +108,14 @@ NDPluginWarp::processCallbacks(NDArray *pArray)
         return;
     }
     switch(pArray->dataType) {
-#define CASE(TYPE) case ND ## TYPE:
-    CASE(Int8)
-    CASE(UInt8)
-    CASE(Int16)
-    CASE(UInt16)
-    CASE(Int32)
-    CASE(UInt32)
-    CASE(Int64)
-    CASE(UInt64)
-    CASE(Float32)
-    CASE(Float64)
-#undef CASE
+    case NDInt8:
+    case NDUInt8:
+    case NDInt16:
+    case NDUInt16:
+    case NDInt32:
+    case NDUInt32:
+    case NDFloat32:
+    case NDFloat64:
         break;
     default:
         asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
@@ -139,98 +125,16 @@ NDPluginWarp::processCallbacks(NDArray *pArray)
     }
     asynPrint(pasynUserSelf, ASYN_TRACE_FLOW, "%s: %s ndarray=%p\n", this->portName, __PRETTY_FUNCTION__, pArray);
 
-    try {
-        epicsTimeStamp before;
-        epicsTimeGetCurrent(&before);
+    pOutput = this->pNDArrayPool->copy(pArray, NULL, true, true, true);
+    
+    (void)pOutput->getInfo(&info);
+    printf("%lu\n",info.nElements);
+    memset(pOutput->pData, 1, 4718592);
 
-        if(!sameShape(info, lastinfo)) {
-            asynPrint(pasynUserSelf, ASYN_TRACEIO_DRIVER, "%s: input shape changes, recompute mapping\n", this->portName);
+    NDPluginDriver::endProcessCallbacks(pOutput, true, true);
 
-            lastinfo = info;
-            recalculate_transform(info);
+    pOutput->release();
 
-            assert(mapping.size() == samp_per_pixel*lastinfo.nElements);
-        }
-
-        int outputmode=0;
-        getIntegerParam(NDWarpOutput, &outputmode);
-
-        ndarray_ptr output;
-
-        if(outputmode==0) {
-            // output transformed image
-
-            output.reset(cloneArray(pNDArrayPool, pArray));
-
-            switch(pArray->dataType) {
-#define CASE(TYPE) case ND ## TYPE: warpit<epics ## TYPE>(pArray, output.get(), &mapping[0], lastinfo.nElements, samp_per_pixel); break;
-            CASE(Int8)
-            CASE(UInt8)
-            CASE(Int16)
-            CASE(UInt16)
-            CASE(Int32)
-            CASE(UInt32)
-            CASE(Int64)
-            CASE(UInt64)
-            CASE(Float32)
-            CASE(Float64)
-#undef CASE
-                    // no default: (error check is above) meant to trigger compiler warning if new types are added
-            }
-
-        } else if(outputmode>=1 && outputmode<=3) {
-            // output mapping distance, x, or y  (to help debug mappings)
-            size_t dims[2] = {lastmap.sizex(), lastmap.sizey()};
-
-            output.reset(pNDArrayPool->alloc(2, dims, NDFloat64, 0, NULL));
-            double *arr = (double*)output->pData;
-            NDArrayInfo_t info;
-            output->getInfo(&info);
-
-            for(size_t oy=0; oy<lastmap.sizey(); oy++) {
-                for(size_t ox=0; ox<lastmap.sizey(); ox++) {
-                    double ix = lastmap.x(ox, oy),
-                           iy = lastmap.y(ox, oy),
-                           dx = ox-ix,
-                           dy = oy-iy;
-
-                    double result = 0.0;
-
-                    switch(outputmode) {
-                    case 1: result = sqrt(dx*dx+dy*dy); break;
-                    case 2: result = dx; break;
-                    case 3: result = dy; break;
-                    }
-
-                    arr[ox*info.xStride + oy*info.yStride] = result;
-                }
-            }
-
-        } else {
-            asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
-                      "%s:: unknown output mode",
-                      this->portName);
-        }
-
-        epicsTimeStamp after;
-        epicsTimeGetCurrent(&after);
-        double delta = epicsTimeDiffInSeconds(&after, &before);
-        setDoubleParam(NDWarpRunTime, delta);
-
-        if(output.get()) {
-            NDPluginDriver::endProcessCallbacks(output.release(), false, true);
-        }
-
-        callParamCallbacks();
-
-        // release() both atemp and output
-    }catch(std::exception& e){
-        lastinfo.nElements = (size_t)-1; // spoil
-
-        asynPrint(pasynUserSelf, ASYN_TRACE_ERROR,
-                  "%s:: unhandled exception in %s : %s",
-                  portName, __PRETTY_FUNCTION__, e.what());
-    }
 }
 
 asynStatus NDPluginWarp::writeFloat64(asynUser *pasynUser, epicsFloat64 value)
@@ -297,148 +201,6 @@ asynStatus NDPluginWarp::writeInt32(asynUser *pasynUser, epicsInt32 value)
     (void)callParamCallbacks(addr, addr);
 
     return ret;
-}
-
-void NDPluginWarp::recalculate_transform(const NDArrayInfo& info)
-{
-    int rawmode=0;
-
-    getIntegerParam(NDWarpMode, &rawmode);
-
-    cur_mode = (mode_t)rawmode;
-
-    switch(cur_mode) {
-    case Nearest:  samp_per_pixel = 1; break;
-    case Bilinear: samp_per_pixel = 4; break;
-    default:
-        throw std::runtime_error("Invalid interpolation mode");
-    }
-    mapping.resize(samp_per_pixel*info.nElements);
-
-    Mapping M;
-    M.resize(info);
-
-    fill_mapping(M);
-
-    // sanitize mapping
-    // replace outside range [0, size-1] with NaN
-    for(size_t j=0; j<M.sizey(); j++) {
-
-        for(size_t i=0; i<M.sizex(); i++) {
-            double x=M.x(i,j), y=M.y(i,j);
-
-            if(x>M.sizex()-1 || x<0.0 || !isfinite(x))
-                M.x(i,j) = std::numeric_limits<double>::quiet_NaN();
-
-            if(y>M.sizey()-1 || y<0.0 || !isfinite(y))
-                M.y(i,j) = std::numeric_limits<double>::quiet_NaN();
-        }
-    }
-
-    // interpolate and collapse user 2D Mapping to 1D 'mapping' (from offset to offset)
-
-    Sample * const S = &mapping[0];
-
-    switch(cur_mode) {
-    case Nearest: {
-        assert(samp_per_pixel==1);
-        for(size_t j=0; j<M.sizey(); j++) {
-
-            for(size_t i=0; i<M.sizex(); i++) {
-                const size_t Ooffset = i*lastinfo.xStride + j*lastinfo.yStride;
-                Sample * const SX = &S[Ooffset];
-                double x=round(M.x(i,j)), y=round(M.y(i,j));
-
-                const size_t Ioffset = x*lastinfo.xStride
-                                     + y*lastinfo.yStride;
-
-                SX->weight = 1.0;
-                SX->index = Ioffset;
-                SX->valid = isfinite(x) && isfinite(y);
-            }
-        }
-    }
-        break;
-    case Bilinear: {
-        assert(samp_per_pixel==4);
-        for(size_t j=0; j<M.sizey(); j++) {
-
-            for(size_t i=0; i<M.sizex(); i++) {
-                const size_t Ooffset = 4*(i*lastinfo.xStride + j*lastinfo.yStride);
-                Sample * const SX = &S[Ooffset];
-
-                double x=M.x(i,j), y=M.y(i,j),
-                       fx=floor(x), fy=floor(y),
-                       cx=ceil(x), cy=ceil(y),
-                       xfact = cx==fx ? 0.0 : (x-fx)/(cx-fx),
-                       yfact = cy==fy ? 0.0 : (y-fy)/(cy-fy);
-                bool valid = isfinite(x) && isfinite(y);
-
-                // for degenerate cases (f_==c_) then
-                // _fact=0.0 or 1.0 gives the same result.
-                // we choose 0.0 arbitrarily
-
-                SX[0].weight = (1.0-xfact)*(1.0-yfact);
-                SX[0].index  = fx*lastinfo.xStride + fy*lastinfo.yStride;
-                SX[0].valid  = valid;
-
-                SX[1].weight = xfact*(1.0-yfact);
-                SX[1].index  = cx*lastinfo.xStride + fy*lastinfo.yStride;
-                SX[1].valid  = valid;
-
-                SX[2].weight = (1.0-xfact)*yfact;
-                SX[2].index  = fx*lastinfo.xStride + cy*lastinfo.yStride;
-                SX[2].valid  = valid;
-
-                SX[3].weight = xfact*yfact;
-                SX[3].index  = cx*lastinfo.xStride + cy*lastinfo.yStride;
-                SX[3].valid  = valid;
-            }
-        }
-    }
-        break;
-    }
-
-    lastmap.swap(M);
-}
-
-void NDPluginWarp::fill_mapping(Mapping &M)
-{
-    double angle = 0.0;
-    double factor[2] = {0.0, 0.0};
-    int center[2] = {0, 0};
-
-    getDoubleParam(NDWarpAngle, &angle);
-    getIntegerParam(NDWarpCenterX, &center[0]);
-    getIntegerParam(NDWarpCenterY, &center[1]);
-    getDoubleParam(NDWarpFactorX, &factor[0]);
-    getDoubleParam(NDWarpFactorY, &factor[1]);
-//    printf("# fill_mapping. center: [%d, %d] angle: %f F: [%f, %f]\n",
-//           center[0], center[1], angle, factor[0], factor[1]);
-
-    const double sina=sin(angle*PI_180), cosa=cos(angle*PI_180);
-
-    // iterate through output image coordinates.
-    // fill in each with input image coordinate
-    for(epicsUInt16 x=0; x<M.sizex(); x++) {
-        for(epicsUInt16 y=0; y<M.sizey(); y++) {
-            double xc = x-center[0];
-            double yc = y-center[1];
-
-            // rotation about center point (Z axis)
-            double temp = xc;
-            xc =  xc*cosa - yc*sina;
-            yc =  temp*sina + yc*cosa;
-
-            // "keystone" correction (rotation about X and/or Y axis)
-            temp = xc;
-            xc += xc*xc*factor[0]   + xc*yc*factor[1];
-            yc += yc*temp*factor[0] + yc*yc*factor[1];
-
-            M.x(x,y) = xc+center[0];
-            M.y(x,y) = yc+center[1];
-        }
-    }
 }
 
 extern "C" int NDWarpConfigure(const char *portName, int queueSize, int blockingCallbacks,
